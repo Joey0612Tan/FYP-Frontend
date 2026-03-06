ef8f082bf55926056385c5f48260e21bdcf1febd
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-03-06 19:00:08 +0800

    Update ConnectDB.php

--- ConnectDB.php
+++ ConnectDB.php
@@ -1,15 +1,12 @@
 <?php
-$host = "mysql-bff3581-fyp-ai-project.d.aivencloud.com";
-$port = 20090;
+$host = "p:mysql-bff3581-fyp-ai-project.d.aivencloud.com";
+$port = 20090; 
 $user = "avnadmin";
 $pass = "AVNS_XKCyPutVMtlvtPhs03c";
 $db   = "defaultdb";    
 
 $conn = mysqli_init();
-
-if (!$conn) {
-    die("mysqli_init failed");
-}
+mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
 
 $success = mysqli_real_connect(
     $conn, 
@@ -23,10 +20,9 @@ $success = mysqli_real_connect(
 );
 
 if (!$success) {
-    die("Connect Error (" . mysqli_connect_errno() . "): " . mysqli_connect_error());
+    die("Connect Error: " . mysqli_connect_error());
 }
 
 mysqli_set_charset($conn, "utf8mb4");
-
 $db = $conn;
 ?>
