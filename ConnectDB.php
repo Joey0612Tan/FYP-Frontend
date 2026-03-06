e0b4ba4499455a719a31c20f99647738169edbc8
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-03-06 18:46:04 +0800

    Update ConnectDB.php

--- ConnectDB.php
+++ ConnectDB.php
@@ -1,17 +1,29 @@
 <?php
 $host = "mysql-bff3581-fyp-ai-project.d.aivencloud.com";
-$port = "20090"; 
+$port = 20090; 
 $user = "avnadmin";
 $pass = "AVNS_XKCyPutVMtlvtPhs03c";
 $db   = "defaultdb";    
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-$success = mysqli_real_connect($conn, $host, $user, $pass, $db, $port);
+if (!$conn) {
+    die("mysqli_init failed");
+}
+
+$success = mysqli_real_connect(
+    $conn, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL 
+);
 
 if (!$success) {
-    die("Connect Error: " . mysqli_connect_error());
+    die("Connect Error (" . mysqli_connect_errno() . "): " . mysqli_connect_error());
 }
 
 $db = $conn;
