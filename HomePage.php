b7cd5ce441fe30a87d2f0bf0a91f643c46f9c261
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-03-10 02:52:43 +0800

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -144,7 +144,7 @@ $result = mysqli_query($conn, $sql);
             height: 300; 
             background: url('https://i.pinimg.com/1200x/86/d5/b9/86d5b9a9c5340a020169f8129f6673af.jpg')
                         no-repeat center 75% / cover;
-            display: flex;
+            display: auto;
             align-items: center;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
         }
@@ -188,7 +188,7 @@ $result = mysqli_query($conn, $sql);
             }
     
             .categories-grid {
-            display: flex;
+            display: auto;
             justify-content: center; 
             gap: 15px;              
             flex-wrap: wrap;
@@ -308,6 +308,7 @@ $result = mysqli_query($conn, $sql);
 <?php include('footer.php'); ?>
 </body>
 </html>
+
 
 
 
