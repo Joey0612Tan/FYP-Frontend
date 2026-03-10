aaf4d7c7143101f73275227594dd141f0a69c907
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-03-11 03:29:22 +0800

    Update add_to_compare.php

--- add_to_compare.php
+++ add_to_compare.php
@@ -1,24 +1,20 @@
 <?php
+
 session_start();
-include('ConnectDB.php');
 
-$p_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-$u_id = 1; 
+$id = $_GET['id'];
 
-if ($p_id > 0) {
-    $check = $conn->prepare("SELECT 1 FROM compare_list WHERE product_id = ? AND user_id = ?");
-    $check->bind_param("ii", $p_id, $u_id);
-    $check->execute();
-    $exists = $check->get_result()->num_rows > 0;
-
-    if ($exists) {
-        http_response_code(409); 
-    } else {
-        $insert = $conn->prepare("INSERT INTO compare_list (product_id, user_id) VALUES (?, ?)");
-        $insert->bind_param("ii", $p_id, $u_id);
-        $insert->execute();
-        http_response_code(200); 
-    }
+if(!isset($_SESSION['compare'])){
+    $_SESSION['compare'] = [];
 }
-exit;
-?>
+
+if(in_array($id, $_SESSION['compare'])){
+
+    http_response_code(409);
+    exit;
+
+}
+
+$_SESSION['compare'][] = $id;
+
+http_response_code(200);
