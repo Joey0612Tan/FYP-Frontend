dc3009cb1ac10cc522128cbcad61319fb3092ea2
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-03-12 02:59:51 +0800

    Update add_to_compare.php

--- add_to_compare.php
+++ add_to_compare.php
@@ -2,18 +2,32 @@
 session_start();
 include('ConnectDB.php');
 
-$id = $_GET['id'];
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    http_response_code(400); // Bad Request
+    echo json_encode(["error" => "Missing ID"]);
+    exit;
+}
 
-if(!isset($_SESSION['compare'])){
+$id = (int)$_GET['id'];
+
+if (!isset($_SESSION['compare'])) {
     $_SESSION['compare'] = [];
 }
 
-if(in_array($id, $_SESSION['compare'])){
-    http_response_code(409);
+if (in_array($id, $_SESSION['compare'])) {
+    http_response_code(409); // Conflict
+    exit;
+}
+
+if (count($_SESSION['compare']) >= 4) {
+    http_response_code(403); // Forbidden
+    echo "Limit reached";
     exit;
 }
 
 $_SESSION['compare'][] = $id;
-http_response_code(200);
 
+http_response_code(200);
+echo json_encode(["success" => true, "count" => count($_SESSION['compare'])]);
+exit;
 ?>
