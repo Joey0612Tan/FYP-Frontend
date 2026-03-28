491e83b54da398599040ec2fb58b793ae940c06a
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-03-29 05:20:01 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -126,7 +126,7 @@ async function handleAICamera(input) {
         const formData = new FormData();
         formData.append('image', compressedBlob, 'image.jpg');
 
-        const response = await fetch('https://fyp-ai-backend-1.onrender.com/visual_search', {
+        const response = await fetch('https://unacted-rae-unregretted.ngrok-free.dev/visual_search', {
             method: 'POST',
             mode: 'cors',
             body: formData
