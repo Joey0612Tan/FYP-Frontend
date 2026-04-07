b6f6723fbf40f125ec784bb2fde5207f6d18cd4c
Joey0612Tan <i23024989@student.newinti.edu.my>
2026-04-08 04:19:22 +0800

    Update remove_from_cart.php

--- remove_from_cart.php
+++ remove_from_cart.php
@@ -3,12 +3,14 @@ session_start();
 include('ConnectDB.php');
 
 if (isset($_GET['id'])) {
-    $product_id = intval($_GET['id']);
+    $cart_id = intval($_GET['id']);  
     $user_id = 1; 
 
-    $stmt = $conn->prepare("DELETE FROM cart WHERE user_id = ? AND product_id = ?");
-    $stmt->bind_param("ii", $user_id, $product_id);
+    $stmt = $conn->prepare("DELETE FROM cart WHERE id = ? AND user_id = ?");
+    $stmt->bind_param("ii", $cart_id, $user_id);
     $stmt->execute();
 }
-exit; 
+
+header("Location: cart.php");
+exit;
 ?>
